48645e0a5de5c4be201ca633a207079dacc12922
Felarn <120136549+Felarn@users.noreply.github.com>
2025-10-21 16:10:23 +0300

    providers moved to separate file

---
+++ terraform/provider.tf
@@ -0,0 +1,11 @@
+provider "yandex" {
+  zone      = var.yc_zone
+  token     = var.yc_token
+  folder_id = var.yc_folder_id
+}
+
+provider "datadog" {
+  api_key = var.datadog_api_key
+  app_key = var.datadog_app_key
+  api_url = var.datadog_api_url
+}
