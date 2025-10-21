240a3e466e49c32a175e80ed79215f4c9386bbfa
Felarn <120136549+Felarn@users.noreply.github.com>
2025-10-21 16:14:20 +0300

    provider.tf fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,4 @@
 terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-    datadog = {
-      source = "DataDog/datadog"
-
-    }
-  }
   required_version = ">= 0.13"
 }
 
-provider "yandex" {
-  zone      = var.yc_zone
-  token     = var.yc_token
-  folder_id = var.yc_folder_id
-}
-
-provider "datadog" {
-  api_key = var.datadog_api_key
-  app_key = var.datadog_app_key
-  api_url = var.datadog_api_url
-}
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+    datadog = {
+      source = "DataDog/datadog"
+
+    }
+  }
+}
+
+
 provider "yandex" {
   zone      = var.yc_zone
   token     = var.yc_token
